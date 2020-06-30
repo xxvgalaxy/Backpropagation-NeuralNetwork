e4dd0ecb3062cff2f447f852a9bec93cfe74a9a5
Jlius Suweno <jliuss@live.com>
2020-06-30 23:22:01 +0700

    Initial commit

--- src/com/gabrielfreire/app/App.java
+++ src/com/gabrielfreire/app/App.java
@@ -8,15 +8,17 @@ public class App {
     public static void main(String[] args) {
         //test code
         float[][] trainingData = new float[][]{
-                new float[]{0, 0},
-                new float[]{0, 1},
-                new float[]{1, 0},
-                new float[]{1, 1},
+                new float[]{(float) 0, 1, 1},
+                new float[]{(float) 0.5, 0, (float) 0.147700815},
+                new float[]{(float) 1, 0, (float) 0.562572759},
+                new float[]{(float) 0, 1, (float) 0.821885914},
+                new float[]{(float) 0.5, 0, 0},
+                new float[]{(float) 1, 0, (float) 0.749417928},
         };
         float[][] trainingResultsOR = new float[][]{new float[]{0}, new float[]{1}, new float[]{1}, new float[]{1}}; //OR
         float[][] trainingResultsAND = new float[][]{new float[]{0}, new float[]{0}, new float[]{0}, new float[]{1}}; //AND
         float[][] trainingResultsXOR = new float[][]{new float[]{0}, new float[]{1}, new float[]{1}, new float[]{0}}; //XOR
-        BackpropagationNeuralNetwork backpropagationNeuralNetwork = new BackpropagationNeuralNetwork(2, 3, 1);
+        BackpropagationNeuralNetwork backpropagationNeuralNetwork = new BackpropagationNeuralNetwork(3, 3, 1);
 
         for(int iteration = 0; iteration < NeuralNetConstants.ITERATIONS; iteration++){
             for(int i = 0; i < trainingResultsXOR.length; i++){
@@ -26,7 +28,7 @@ public class App {
             for(int i = 0; i < trainingResultsXOR.length; i++){
                 float[] t = trainingData[i];
                 System.out.println("Num of iterations: " + (iteration + 1));
-                System.out.printf("%.1f, %.1f --> %.3f\n", t[0], t[1], backpropagationNeuralNetwork.run(t)[0]);
+                System.out.printf("%.1f, %.1f, %.1f --> %.3f\n", t[0], t[1], t[2], backpropagationNeuralNetwork.run(t)[0]);
             }
         }
     }
