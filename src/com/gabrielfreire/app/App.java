cb9f4892d30473556cac0b415d2b7449387bcec0
rikinotnull <riki@cyberindo-sinergi.com>
2020-07-01 11:26:16 +0700

    add galat, target, and change trainingData

--- src/com/gabrielfreire/app/App.java
+++ src/com/gabrielfreire/app/App.java
@@ -8,16 +8,23 @@ public class App {
     public static void main(String[] args) {
         //test code
         float[][] trainingData = new float[][]{
-                new float[]{(float) 0, 1, 1},
-                new float[]{(float) 0.5, 0, (float) 0.147700815},
-                new float[]{(float) 1, 0, (float) 0.562572759},
-                new float[]{(float) 0, 1, (float) 0.821885914},
-                new float[]{(float) 0.5, 0, 0},
-                new float[]{(float) 1, 0, (float) 0.749417928},
+                new float[]{(float) 0, 1, (float) 0.88700},
+                new float[]{(float) 0.5, 0, (float) 0.20540},
+                new float[]{(float) 1, 0, (float) 0.53718},
+                new float[]{(float) 0, 1, (float) 0.74456},
+                new float[]{(float) 0.5, 0, (float) 0.08728},
+                new float[]{(float) 1, 0, (float) 0.68661},
         };
         float[][] trainingResultsOR = new float[][]{new float[]{0}, new float[]{1}, new float[]{1}, new float[]{1}}; //OR
         float[][] trainingResultsAND = new float[][]{new float[]{0}, new float[]{0}, new float[]{0}, new float[]{1}}; //AND
-        float[][] trainingResultsXOR = new float[][]{new float[]{0}, new float[]{1}, new float[]{1}, new float[]{0}}; //XOR
+        float[][] trainingResultsXOR = new float[][]{
+                new float[]{(float)0.08721},
+                new float[]{(float)0.38691},
+                new float[]{(float)0.26973},
+                new float[]{0},
+                new float[]{(float)0.81090},
+                new float[]{1}
+        };
         BackpropagationNeuralNetwork backpropagationNeuralNetwork = new BackpropagationNeuralNetwork(3, 3, 1);
 
         for(int iteration = 0; iteration < NeuralNetConstants.ITERATIONS; iteration++){
@@ -25,10 +32,16 @@ public class App {
                 backpropagationNeuralNetwork.train(trainingData[i], trainingResultsXOR[i], NeuralNetConstants.LEARNING_RATE, NeuralNetConstants.MOMENTUM);
             }
             System.out.println();
+            int count = 0;
             for(int i = 0; i < trainingResultsXOR.length; i++){
                 float[] t = trainingData[i];
+                float result = backpropagationNeuralNetwork.run(t)[0];
+                count++;
                 System.out.println("Num of iterations: " + (iteration + 1));
-                System.out.printf("%.1f, %.1f, %.1f --> %.3f\n", t[0], t[1], t[2], backpropagationNeuralNetwork.run(t)[0]);
+                System.out.printf("Data ke %d, X1=%.1f, X2=%.1f, X3=%f --> OUTPUT=%f : TARGET=%f : GALAT=%f \n",count, t[0], t[1], t[2], result, trainingResultsXOR[i][0], Math.abs(result - trainingResultsXOR[i][0]) );
+                if(count>5){
+                    count = 0;
+                }
             }
         }
     }
